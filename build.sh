145e59e1b0abb1a760c28d4b485aa2c048eec82d
Waleedanwar01 <waleeddogare@gmail.com>
2026-01-31 18:24:02 +0500

    Update build.sh with npm build and add render.yaml config

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Build Tailwind CSS
+cd theme/static_src
+npm install
+npm run build
+cd ../..
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
